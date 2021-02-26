b238797401ef418e897b7652b1a796e39e322eae
Ray <fengruipeng>
2021-02-26 11:12:29 +0800

    websocket配置

---
+++ src/main/java/com/zte/msg/alarmcenter/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.zte.msg.alarmcenter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author frp
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
